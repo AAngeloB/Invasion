040cda8d9dcd68f3b771f7536b4cbd7bac58f4ea
AAngeloB <asvpangelob@gmail.com>
2017-11-22 14:36:30 -1000

    invasion

---
+++ app.js
@@ -0,0 +1,33 @@
+var aliens=10000;
+var greetings;
+var x=2500;
+//*green*//
+var y=4500;
+//*red*//
+var z=3000;
+//*blue*//
+
+var total1= x+y;
+var total2= y+z;
+
+
+
+function alienInvasion(x,y,z){
+	if(aliens = 10000){
+		greetings= "YUHPS"
+	}
+	
+	else if(x > 5000){
+		greetings="MOTHER OF GOD, not the green ones"
+	}
+
+	else if(total1 > 8000){
+		greetings="DOOOOOM!"
+	}
+
+	else(total2 > 3000){
+		greetings="We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!"
+	}
+}
+
+console.log(greetings)
